e34d1484276d2b47c954cb4514875b1925b00f1a
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-03 07:10:16 +0100

    add 05

---
+++ 05/solve.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+using static System.Console;
+
+string[] track = File.ReadAllLines("input");
+Debug.Assert(track.Length > 0);
+Debug.Assert(track[0].Length > 0);
+Debug.Assert(track.All(s => s.Length == track[0].Length));
+Debug.Assert(track.All(s => s.All(c => c == '.' || c == '#')));
+
+long trees = 0;
+for (int i = 0; i != track.Length; ++i) {
+  if (track[i][i * 3 % track[i].Length] == '#') ++trees;
+}
+WriteLine(trees);
