da3b357030004e9926d9559139569f1ab98bcc32
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-03 07:26:09 +0100

    cleanup

--- 05/solve.cs
+++ 05/solve.cs
@@ -5,10 +5,10 @@ using System.Linq;
 using static System.Console;
 
 string[] track = File.ReadAllLines("input");
-Debug.Assert(track.Length > 0);
-Debug.Assert(track[0].Length > 0);
-Debug.Assert(track.All(s => s.Length == track[0].Length));
-Debug.Assert(track.All(s => s.All(c => c is '.' or '#')));
+Trace.Assert(track.Length > 0);
+Trace.Assert(track[0].Length > 0);
+Trace.Assert(track.All(s => s.Length == track[0].Length));
+Trace.Assert(track.All(s => s.All(c => c is '.' or '#')));
 
 long trees = 0;
 for (int i = 0; i != track.Length; ++i) {
