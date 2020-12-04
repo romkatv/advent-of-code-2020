4e51e9f8d0c00bda2910971ead3b207223300dfd
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-04 09:25:22 +0100

    add 07

---
+++ 07/solve.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using static System.Console;
+
+int res = -1;
+HashSet<string> req = new();
+
+foreach (string line in new[] {""}.Concat(File.ReadLines("input")).Append("")) {
+  if (line == "") {
+    if (req.Count == 0) ++res;
+    req = new() { "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
+    continue;
+  }
+  foreach (string kv in line.Split(' ')) {
+    req.Remove(kv.Substring(0, kv.IndexOf(':')));
+  }
+}
+
+WriteLine(res);
