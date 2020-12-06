f11bfc0c29e7bb3fbbd6e4b3cfe74657f519e463
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-06 16:39:47 +0100

    add 11

---
+++ 11/solve.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using static System.Console;
+
+int res = 0;
+HashSet<char> ans = new();
+
+foreach (string line in File.ReadLines("input").Append("")) {
+  if (line == "") {
+    res += ans.Count;
+    ans.Clear();
+  } else {
+    foreach (char c in line) ans.Add(c);
+  }
+}
+
+WriteLine(res);
