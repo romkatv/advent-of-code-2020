66eab5cebb813c60848b33fa31cfdd32204e9dc4
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-06 16:39:52 +0100

    add 12

---
+++ 12/solve.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static System.Console;
+
+int res = 0;
+List<char> ans = null;
+
+foreach (string line in File.ReadLines("input").Append("")) {
+  if (line == "") {
+    res += ans.Count;
+    ans = null;
+  } else if (ans == null) {
+    ans = line.ToList();
+  } else {
+    HashSet<char> chars = line.ToHashSet();
+    ans.RemoveAll(c => !chars.Contains(c));
+  }
+}
+
+WriteLine(res);
