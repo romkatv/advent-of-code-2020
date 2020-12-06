00145704adf9c43f89e8dde0711a46fe9aaa4f9c
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-06 15:36:35 +0100

    add 09

---
+++ 09/solve.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+WriteLine(File.ReadLines("input").Select(ParseId).Max());
+
+int ParseId(string s) => ToInt32(
+    s.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'),
+    2);
