491666375387f5f1587c07fc708da1f4a3bbc7eb
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-06 15:36:44 +0100

    add 10

---
+++ 10/solve.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Linq;
+using static System.Console;
+using static System.Convert;
+
+int? prev = null;
+foreach (int id in File.ReadLines("input").Select(ParseId).OrderBy(x => x)) {
+  if (id == prev + 2) {
+    WriteLine(id - 1);
+    return 0;
+  }
+  prev = id;
+}
+
+return 1;
+
+int ParseId(string s) => ToInt32(
+    s.Replace('F', '0').Replace('B', '1').Replace('L', '0').Replace('R', '1'),
+    2);
