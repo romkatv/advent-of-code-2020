c86df7161aee701b3e3e3c14096b57c7460fbb36
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-09 10:42:03 +0100

    reimplement 04 in c#

---
+++ 04/solve.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using static System.Console;
+using static System.IO.File;
+
+WriteLine(ReadLines("input").Count((string s) => {
+  Match m = Regex.Match(s, @"^(\d+)-(\d+) (.): (.*)$");
+  string Capture(int i) => m.Groups[i].Value;
+  bool Eq(int i) => Capture(4)[int.Parse(Capture(i)) - 1] == Capture(3)[0];
+  Trace.Assert(m.Success);
+  return Eq(1) != Eq(2);
+}));
