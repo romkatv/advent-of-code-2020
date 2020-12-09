d17c64fd34117757b5a174aee265fdbbda4ddc97
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-09 10:22:56 +0100

    reimplement 01 in c#

---
+++ 01/solve.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using static System.Console;
+using static System.IO.File;
+
+HashSet<int> seen = new();
+foreach (int n in ReadLines("input").Select(int.Parse)) {
+  int m = 2020 - n;
+  if (seen.Contains(m)) {
+    WriteLine(n * m);
+    return;
+  }
+  Trace.Assert(seen.Add(n));
+}
