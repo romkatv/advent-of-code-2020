448f8186c2893dea9708aa84d2ac4d1aa8f0b432
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-10 06:55:14 +0100

    add 19

---
+++ 19/solve.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using static System.Console;
+using static System.IO.File;
+
+int prev = 0;
+int[] diff = new int[3];
+
+foreach (int cur in ReadLines("input").Select(int.Parse).OrderBy(x => x)) {
+  ++diff[cur - prev - 1];
+  prev = cur;
+}
+
+WriteLine(diff[0] * (diff[2] + 1));
