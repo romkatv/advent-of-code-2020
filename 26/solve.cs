224b9ecd14a3848d868a61e8b22fd461d7529d6d
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-13 11:04:09 +0100

    add 26

---
+++ 26/solve.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+using static System.IO.File;
+
+long x = 0, y = -1, n = 1;
+
+foreach (string s in ReadAllLines("input")[1].Split(',')) {
+  ++y;
+  if (s == "x") continue;
+  int m = int.Parse(s);
+  while ((x + y) % m != 0) x = x += n;
+  n *= m;
+}
+
+WriteLine(x);
