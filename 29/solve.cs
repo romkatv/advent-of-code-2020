ec36853bb9b72b419e24b6c419ab76c9d3b304c1
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-15 15:12:27 +0100

    add 29

---
+++ 29/solve.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+using static System.IO.File;
+
+const int N = 2020;
+
+List<int> nums = ReadLines("input")
+    .First()
+    .Split(',')
+    .Select(int.Parse)
+    .ToList();
+
+for (int n; (n = nums.Count) < N;) {
+  int x = nums.Last();
+  int i = nums.FindLastIndex(n - 2, n - 1, y => x == y);
+  nums.Add(i >= 0 ? n - i - 1 : 0);
+}
+
+WriteLine(nums[N - 1]);
