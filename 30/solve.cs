ed84c3a496e269760978ff6db92b79baf33e25a8
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-15 21:31:58 +0100

    add 30

---
+++ 30/solve.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using static System.Console;
+using static System.IO.File;
+
+const int N = 30000000;
+
+int i = 1;
+int last = 0;
+int[] nums = new int[N];
+
+foreach (int x in ReadLines("input").First().Split(',').Select(int.Parse)) {
+  last = nums[x];
+  nums[x] = i++;
+}
+
+for (; ; ++i) {
+  int x = last == 0 ? 0 : i - last - 1;
+  if (i == N) {
+    WriteLine(x);
+    break;
+  }
+  last = nums[x];
+  nums[x] = i;
+}
