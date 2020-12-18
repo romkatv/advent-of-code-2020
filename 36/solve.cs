2086f10aac3ddde5749941a7206afb4d6919396a
Roman Perepelitsa <roman.perepelitsa@gmail.com>
2020-12-18 12:17:50 +0100

    add 36

---
+++ 36/solve.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using static System.Console;
+using static System.IO.File;
+using static System.Linq.Enumerable;
+
+long res = ReadLines("input")
+    .Select(expr => Eval(expr.Replace("(", "( ").Replace(")", " )").Split(' ')))
+    .Sum();
+WriteLine(res);
+
+long Eval(string[] tokens) {
+  int i = 0;
+  return Expr();
+
+  long Expr() {
+    long res = Term();
+    while (i != tokens.Length) {
+      if (tokens[i++] == "*") {
+        res *= Term();
+      } else {
+        break;
+      }
+    }
+    return res;
+  }
+
+  long Factor() => tokens[i++] == "(" ? Expr() : long.Parse(tokens[i-1]);
+
+  long Term() {
+    long res = Factor();
+    while (i != tokens.Length && tokens[i] == "+") {
+      ++i;
+      res += Factor();
+    }
+    return res;
+  }
+}
